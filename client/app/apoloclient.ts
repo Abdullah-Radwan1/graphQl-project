54d210a5884fcb7e9dda3ce50cc518a7cb25e9e9
abdullah <abdallahbeedo855@gmail.com>
2025-03-17 23:50:39 +0200

    adding env vars

--- client/app/apoloclient.ts
+++ client/app/apoloclient.ts
@@ -1,9 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export function initializeApollo() {
- return new ApolloClient({
-  uri: "http://localhost:4000/graphql", // Replace with your GraphQL server URI
-  cache: new InMemoryCache(),
-  credentials: "include", // This ensures cookies are sent with requests
- });
+  return new ApolloClient({
+    uri: process.env.URI, // Replace with your GraphQL server URI
+    cache: new InMemoryCache(),
+    credentials: "include", // This ensures cookies are sent with requests
+  });
 }
--- client/components/Provider.tsx
+++ client/components/Provider.tsx
@@ -4,19 +4,19 @@ import React from "react";
 
 // Create and export Apollo Client instance
 export const client = new ApolloClient({
- uri: "http://localhost:4000/graphql",
- cache: new InMemoryCache(),
- credentials: "include",
+  uri: process.env.NEXT_PUBLIC_URI,
+  cache: new InMemoryCache(),
+  credentials: "include",
 });
 
 // Optional: Create an initializeApollo function if dynamic reinitialization is needed
 export function initializeApollo() {
- return client;
+  return client;
 }
 
 // Provider component for wrapping your app
 const Provider = ({ children }: { children: React.ReactNode }) => {
- return <ApolloProvider client={client}>{children}</ApolloProvider>;
+  return <ApolloProvider client={client}>{children}</ApolloProvider>;
 };
 
 export default Provider;
